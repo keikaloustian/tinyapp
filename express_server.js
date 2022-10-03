2fbd270bcc661dc92ae09a5ed0b671c73e915218
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-03 14:56:27 -0700

    Add second route to individual url

--- express_server.js
+++ express_server.js
@@ -5,9 +5,14 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  "b2xCn2": "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render('urls_show', templateVars);
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
