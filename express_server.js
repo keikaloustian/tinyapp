5123b63c77f17c44912f6d65bbb82ad39b07e0a3
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-04 16:34:22 -0700

    Implement editing

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require('morgan');
 const app = express();
 const PORT = 8080;
 const shortIdLength = 6;
@@ -12,11 +13,24 @@ function generateRandomString(length) {
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "asdasd": "http://www.example.com/"
 };
+
+
+
+
+
+app.post('/urls/:id', (req, res) => {
+  urlDatabase[req.params.id] = req.body.newLongURL;
+  res.redirect('/urls');
+});
+
+
 
 app.post('/urls/:id/delete', (req, res) => {
   const idToDelete = req.params.id;
@@ -24,6 +38,8 @@ app.post('/urls/:id/delete', (req, res) => {
   res.redirect('/urls');
 });
 
+
+// Route to create new TinyURL
 app.post('/urls', (req, res) => {
   const newID = generateRandomString(shortIdLength);
   urlDatabase[newID] = req.body.longURL;
@@ -32,32 +48,39 @@ app.post('/urls', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
+
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
+
 
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
 });
 
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
