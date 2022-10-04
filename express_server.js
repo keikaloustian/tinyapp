f55c64550231a1bf73b8f32634eb0e5cee1f67e5
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-03 18:11:16 -0700

    Implement new url form with GET and POST routes

--- express_server.js
+++ express_server.js
@@ -2,12 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+function generateRandomString() {
+  let randNum = Math.random();
+  let string = randNum.toString(36);
+  return string.slice(2, 8);
+};
+
+
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
